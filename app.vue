0420ed2b8bf8fdc3e90d28c4a9c22f995fe62787
GustavoHSS46 <116316734+GustavoHSS46@users.noreply.github.com>
2023-03-08 13:48:27 -0300

    Start 🚀

---
+++ app.vue
@@ -0,0 +1,226 @@
+<template>
+    <div class="container">
+        <h1>Navbar Pikka</h1>
+        <NuxtPage/>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap");
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+#__nuxt {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+}
+
+/* Ultrawide Monitors */
+@media screen and (min-width: 1921px) and (max-width: 2560px) {
+    .container {
+        width: 1920px;
+        border: 2px solid red;
+    }
+    h1 {
+        font-family: "Bebas Neue", cursive !important;
+        font-size: 4rem;
+        color: red;
+    }
+
+    h2 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 400;
+        font-size: 2.5rem;
+    }
+
+    h3 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 300;
+        font-size: 2rem;
+    }
+
+    h4 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 200;
+        font-size: 1.5rem;
+    }
+
+    h5 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 100;
+        font-size: 1rem;
+    }
+}
+
+/* FullHD Monitors */
+@media screen and (min-width: 1441px) and (max-width: 1920px) {
+    .container {
+        width: 1440px;
+        border: 2px solid green;
+    }
+    h1 {
+        font-family: "Bebas Neue", cursive !important;
+        font-size: 4rem;
+    }
+
+    h2 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 400;
+        font-size: 2.5rem;
+    }
+
+    h3 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 300;
+        font-size: 2rem;
+    }
+
+    h4 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 200;
+        font-size: 1.5rem;
+    }
+
+    h5 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 100;
+        font-size: 1rem;
+    }
+}
+
+/* HD Monitors */
+@media screen and (min-width: 1024px) and (max-width: 1440px) {
+    .container {
+        width: 1024px;
+        border: 2px solid blue;
+    }
+    h1 {
+        font-family: "Bebas Neue", cursive !important;
+        font-size: 4rem;
+    }
+
+    h2 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 400;
+        font-size: 2.5rem;
+    }
+
+    h3 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 300;
+        font-size: 2rem;
+    }
+
+    h4 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 200;
+        font-size: 1.5rem;
+    }
+
+    h5 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 100;
+        font-size: 1rem;
+    }
+}
+
+/* Tablets Monitors */
+@media screen and (min-width: 768px) and (max-width: 1023px) {
+    .container {
+        width: 100%;
+        border: 2px solid purple;
+    }
+
+    h1 {
+        font-family: "Bebas Neue", cursive !important;
+        font-size: 2.5rem;
+    }
+
+    h2 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 400;
+        font-size: 2rem;
+    }
+
+    h3 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 300;
+        font-size: 1.5rem;
+    }
+
+    h4 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 200;
+        font-size: 1rem;
+    }
+
+    h5 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 100;
+        font-size: 0.5rem;
+    }
+}
+
+/* Phones Monitors */
+@media screen and (min-width: 220px) and (max-width: 767px) {
+    .container {
+        width: 100%;
+        padding-left: 10px;
+        padding-right: 10px;
+        border: 2px solid yellow;
+    }
+
+    h1 {
+        font-family: "Bebas Neue", cursive !important;
+        font-size: 2rem;
+    }
+
+    h2 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 400;
+        font-size: 1.5rem;
+    }
+
+    h3 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 300;
+        font-size: 1rem;
+    }
+
+    h4 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 200;
+        font-size: 0.75rem;
+    }
+
+    h5 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 100;
+        font-size: 0.5rem;
+    }
+}
+</style>
