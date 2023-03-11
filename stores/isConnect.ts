3fab5b65b0211fb14425a282583af73ebb39a398
GustavoHSS46 <116316734+GustavoHSS46@users.noreply.github.com>
2023-03-10 23:54:52 -0300

    🔧 Profile picture bug fix

--- stores/isConnect.ts
+++ stores/isConnect.ts
@@ -4,23 +4,43 @@ import { getAuth } from "firebase/auth";
 
 export const useConexion = definePiniaStore('connected', {
     state: () => ({
-        isConnected: useLocalStorage('conexion', false),
-        name: useStorage('macaco', ''), 
+        isConnected: useLocalStorage('isConnected', false),
+        name: useStorage('name', ''),
+        profilePic: useStorage('profilePic', '')
     }),
     actions: {
         connect() {
             const auth = getAuth();
             const user = auth.currentUser;
             if (user) {
-                useLocalStorage('macaco',  user.displayName)
-                useLocalStorage('conexion',  true)
                 this.name = user.displayName!
+                this.profilePic = user.photoURL!
                 this.isConnected = true
+                useLocalStorage('profilePic', this.profilePic, { mergeDefaults: true })
+                useLocalStorage('isConnected', this.isConnected, { mergeDefaults: true })
+                useLocalStorage('name', this.name, { mergeDefaults: true })
             } else {
-                useLocalStorage('conexion', false)
-                this.isConnected = false
+                this.initialize()
             }
+        },
+        logOut() {
+            const auth = getAuth();
+            auth.signOut().then(() => {
+                console.log("sign out nigga")
+                this.initialize()
+                console.log("logged out")
+            })
+        },
+        initialize() {
+            console.log("logged out123123123")
+            useLocalStorage('macaco', '', { mergeDefaults: true }).value = null
+            useLocalStorage('name', '', { mergeDefaults: true }).value = null
+            useLocalStorage('profilePic', 'default', { mergeDefaults: true }).value = null
+            useLocalStorage('isConnected', false, { mergeDefaults: true }).value = null
+            this.name = ''
+            this.profilePic = ''
+            this.isConnected = false
         }
     },
-    
+
 })
